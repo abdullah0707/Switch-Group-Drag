6ef26e770533f9eb670827175b0641bc9c161cad
Abdullah Mohammed <abdullahelgammal25@gmail.com>
2024-10-02 14:00:04 +0300

    up 1

---
+++ story_content/triggers.js
@@ -0,0 +1,45 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68d6wcfi9Bs":
+        Script1();
+        break;
+      case "6nGO3g7g3uG":
+        Script2();
+        break;
+      case "6MjEaDwk7PU":
+        Script3();
+        break;
+      case "5dM7DQS31SC":
+        Script4();
+        break;
+      case "68baoze8yrX":
+        Script5();
+        break;
+      case "5smX1QLVDQ9":
+        Script6();
+        break;
+      case "5ZVdpglXlct":
+        Script7();
+        break;
+      case "6ZBdiFOW21Z":
+        Script8();
+        break;
+      case "5YosewFSdwx":
+        Script9();
+        break;
+      case "5u0mBTgzC8B":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,58 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script2 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script3 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script4 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script5 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script6 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script7 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script8 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+window.Script9 = function()
+{
+  DS.appState.onToggleVolume(0);
+}
+
+window.Script10 = function()
+{
+  DS.appState.onToggleVolume(1);
+}
+
+};
